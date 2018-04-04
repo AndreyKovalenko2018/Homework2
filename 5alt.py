e4aad556898020f27c915c15367a8e0e75588fe5
andreykovalenko2018 <andrey.kovalenko.kyiv@gmail.com>
2018-04-04 22:33:37 +0300

    num5

--- 5alt.py
+++ 5alt.py
@@ -1,16 +1,14 @@
+print('5)')
 a=int(input('a='))
 b=int(input('b='))
 c=int(input('c='))
-
 print(a,b,c)
-
 if not a<b:
     a,b=b,a
 if not a<c:
     c,a=a,c
 if not b<c:
     c,b=b,c
-
 print('a=',a)
 print('b=',b)
 print('c=',c)
